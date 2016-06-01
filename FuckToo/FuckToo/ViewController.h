0e2269b89ebb84546112e97d955ced6294fbf88d
许跃辉 <137536032@qq.com>
2016-06-01 22:13:07 +0800

    zhen
    123

---
+++ FuckToo/FuckToo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FuckToo
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 许跃辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FuckToo/FuckToo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FuckToo
+//
+//  Created by student on 16/6/1.
+//  Copyright © 2016年 许跃辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
